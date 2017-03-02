b73bb9c5eacb6ecf2a20e29c226f1de90746b892
GarenLiang <ideabreeze@gmail.com>
2017-03-01 19:20:19 -0800

    IndexRoute

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import reducers from './reducers';
 import routes from './routes';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
