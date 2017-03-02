665f432b0f8d97a88ed847460f58e5a7419d949f
GarenLiang <ideabreeze@gmail.com>
2017-03-02 15:27:43 -0800

    New Link

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -9,12 +10,17 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+      List of posts
+      </div>
     );
   }
 }
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchPosts }, dispatch);
-}
 
-export default connect(null, mapDispatchToProps)(PostsIndex);
+
+export default connect(null, { fetchPosts })(PostsIndex);
