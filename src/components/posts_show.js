c0df9520129b1c4399ceaf3c27f47397610752ad
GarenLiang <ideabreeze@gmail.com>
2017-03-07 16:37:27 -0800

    Delete Function

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
-
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
-
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+        .then(() => { this.context.router.push('/'); });
+  }
   render() {
     const { post } = this.props;
     if (!post) {
@@ -15,6 +22,11 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back to Index</Link>
+        <button className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -26,4 +38,4 @@ class PostsShow extends Component {
 function mapStateToProps(state) {
   return { post: state.posts.post };
 }
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
